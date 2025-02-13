c8b3fbcf416c2a4b7bf4ed92ce26ac9b4140f722
odagr81 <orlando.latham@gmail.com>
2025-02-13 11:23:45 -0500

    Fixing typos

--- main.tf
+++ main.tf
@@ -37,7 +37,7 @@ resource "aws_security_group" "blog" {
   vpc_id = data.aws_vpc.default.id
 }
 
-resource ""aws_security_group_rules" "blog_http_in"{
+resource "aws_security_group_rules" "blog_http_in"{
   type              = "ingress"
   from_port         = "80"
   to_port           = "80"
@@ -47,7 +47,7 @@ resource ""aws_security_group_rules" "blog_http_in"{
   security_group_id = aws_security_group.blog.id
 }
 
-resource ""aws_security_group_rules" "blog_https_in"{
+resource "aws_security_group_rules" "blog_https_in"{
   type              = "ingress"
   from_port         = "443"
   to_port           = "443"
@@ -57,7 +57,7 @@ resource ""aws_security_group_rules" "blog_https_in"{
   security_group_id = aws_security_group.blog.id
 }
 
-resource ""aws_security_group_rules" "blog_http_out"{
+resource "aws_security_group_rules" "blog_http_out"{
   type              = "egress"
   from_port         = "0"
   to_port           = "0"
